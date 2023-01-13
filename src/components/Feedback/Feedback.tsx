df2c47b7bfbb2f971a87691b8de7bf33295ef5e7
Vadim Belousov <thevadiratti@yandex.ru>
2023-01-13 20:20:21 +0500

    добавить модальное окно

--- src/components/Feedback/Feedback.tsx
+++ src/components/Feedback/Feedback.tsx
@@ -1,10 +1,31 @@
 import './Feedback.scss';
+import { messages } from './data';
 
 export default function Feedback() {
   return (
     <div className='modal'>
-      <div className='smilesCnt'></div>
+      <div className='smilesCnt'>
+        <span className='smile'>&#128077;</span>
+        <span className='smile'>&#128078;</span>
+        <span className='smile'>&#128075;</span>
+        <span className='smile'>&#128578;</span>
+        <span className='smile'>&#128542;</span>
+        <span className='smile'>&#129315;</span>
+        <span className='smile'>&#128556;</span>
+        <span className='smile'>&#128561;</span>
+        <span className='smile'>&#128525;</span>
+        <span className='smile'>&#128420;</span>
+      </div>
       <input type='text' placeholder='Обратная связь' className='input'></input>
+      <div className='feedbackTape'>
+        {messages.map(item => {
+          return(
+            <div className='message'>
+              <p className='messageText'>{item}</p>
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
