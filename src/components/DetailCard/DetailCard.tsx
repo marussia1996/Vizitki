6ffcb7350c6c5fd2bffd8d5fdf85e968ec319224
Vadim Belousov <thevadiratti@yandex.ru>
2023-01-13 11:59:45 +0500

    закончить верстку DetailCard

--- src/components/DetailCard/DetailCard.tsx
+++ src/components/DetailCard/DetailCard.tsx
@@ -1,19 +1,42 @@
-import Styles from './DetailCard.module.css';
+import './DetailCard.scss';
+import { useEffect, useState } from 'react';
+import { TPageType } from '../../services/types/types';
+import defaultLine from '../../images/Line/default.svg';
+import romanticLine from '../../images/Line/romantic.svg';
+import daringLine from '../../images/Line/daring.svg';
 
 type TProps = {
+  theme: TPageType;
   heading: string;
   text: string;
   image?: string;
 };
 
-export default function DetailCard ({heading, text, image}: TProps) {
+export default function DetailCard({ theme, heading, text, image }: TProps) {
+  const [themeType, setTheme] = useState(defaultLine);
+
+  useEffect(() => {
+    switch(theme) {
+      case 'default':
+        break;
+      case 'romantic':
+        setTheme(romanticLine);
+        break;
+      case 'daring':
+        setTheme(daringLine);
+        break;
+    }
+  });
+
   return (
-    <div className={Styles.card}>
-      <h3 className={Styles.heading}>{heading}</h3>
+    <div className='card'>
+      <div className='line' style={{ backgroundImage: `url(${themeType})` }}></div>
+      <h3 className='heading'>{heading.toUpperCase()}</h3>
       {image && (
-        <div className={Styles.image} style={{backgroundImage: `url(${image})`}}></div>
+        <div className='image' style={{ backgroundImage: `url(${image})` }}></div>
       )}
-      <p className={Styles.text}>{text}</p>
+      <p className='text'>{text}</p>
+      {/* Здесь будет компонент CommentIcon */}
     </div>
   )
 }
