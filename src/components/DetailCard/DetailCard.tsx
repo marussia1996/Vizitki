103ee3aadd4bdad958a7700fab9505d9d3c1c240
Vadim Belousov <thevadiratti@yandex.ru>
2023-01-13 17:52:00 +0500

    добавить инпут

--- src/components/DetailCard/DetailCard.tsx
+++ src/components/DetailCard/DetailCard.tsx
@@ -5,6 +5,7 @@ import defaultLine from '../../images/Line/default.svg';
 import romanticLine from '../../images/Line/romantic.svg';
 import daringLine from '../../images/Line/daring.svg';
 import { CommentIcon } from '../CommentIcon/CommentIcon';
+import Feedback from '../Feedback/Feedback';
 
 type TProps = {
   theme?: TThemeProfile;
@@ -15,6 +16,7 @@ type TProps = {
 
 export default function DetailCard({ theme = TThemeProfile.DEFAULT, heading, text, image }: TProps) {
   const [themeType, setTheme] = useState(defaultLine);
+  const [isOpenFeedback, setFeedbackState] = useState(false);
 
   useEffect(() => {
     if(theme !== TThemeProfile.DEFAULT) {
@@ -29,6 +31,10 @@ export default function DetailCard({ theme = TThemeProfile.DEFAULT, heading, tex
     }
   });
 
+  const feedbackHandle = () => {
+    setFeedbackState(!isOpenFeedback);
+  }
+
   return (
     <div className='card'>
       <div className='line' style={{ backgroundImage: `url(${themeType})` }}></div>
@@ -40,6 +46,10 @@ export default function DetailCard({ theme = TThemeProfile.DEFAULT, heading, tex
         <div className='image' style={{ backgroundImage: `url(${image})` }}></div>
       )}
       <p className='text'>{text}</p>
+      <div className='cnt' onClick={feedbackHandle}>
+        <CommentIcon color='pink' />
+      </div>
+      {isOpenFeedback && <Feedback />}
     </div>
   )
 }
---
+++ src/components/Feedback/Feedback.tsx
@@ -0,0 +1,10 @@
+import './Feedback.scss';
+
+export default function Feedback() {
+  return (
+    <div className='modal'>
+      <div className='smilesCnt'></div>
+      <input type='text' placeholder='Обратная связь' className='input'></input>
+    </div>
+  )
+}
