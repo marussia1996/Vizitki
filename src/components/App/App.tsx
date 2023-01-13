7d3799b237679ac218c1b92790d760241dabe430
Vadim Belousov <thevadiratti@yandex.ru>
2023-01-13 12:02:17 +0500

    подготовка к слиянию

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,24 +1,23 @@
 import React, { FC } from 'react';
-import { Route, Switch , useLocation} from 'react-router-dom';
+import { Route, Switch, useLocation } from 'react-router-dom';
 import { TLocation } from '../../services/types/types';
 import './App.scss';
-import {Header} from '../Header/Header';
+import { Header } from '../Header/Header';
 import {UserCard} from '../UserCard/UserCard';
-import DetailCard from '../DetailCard/DetailCard';
 import { Button } from '../Button/Button';
 
-export const App:FC = () => {
+export const App: FC = () => {
   const location = useLocation<TLocation>();
   return (
     <div className='app'>
       <Switch location={location}>
         <Route exact path="/maria">
           <h1>Привет, Мария</h1>
-          <Button className='buttonLarge' onClick={()=>{console.log('send')}} disabled={false}>Войти с Яндекс ID</Button>
-          <Button className='buttonSmall' onClick={()=>{console.log('send')}} disabled={true}>Выберите файл</Button>
+          <Button className='buttonLarge' onClick={() => { console.log('send') }} disabled={false}>Войти с Яндекс ID</Button>
+          <Button className='buttonSmall' onClick={() => { console.log('send') }} disabled={true}>Выберите файл</Button>
         </Route>
         <Route exact path="/vadim">
-          <DetailCard theme='default' heading='Увлечения' text='Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, ullam cum pariatur numquam impedit accusantium totam incidunt qui! Autem vel quisquam recusandae minus sit eius aperiam sequi debitis ipsum incidunt.' />
+          <h1>Привет, Вадим</h1>
         </Route>
         <Route exact path="/vladislav">
           <h1>Привет, Владислав</h1>
