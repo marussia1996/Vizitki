3fee95f2af19a8c9aebf831ae07d214bb2427fe7
Vadim Belousov <thevadiratti@yandex.ru>
2023-01-15 17:41:48 +0500

    подготовить к слиянию

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,28 +3,25 @@ import { Route, Switch, useLocation } from 'react-router-dom';
 import AlexeyM from '../../pages/AlexeyM/AlexeyM';
 import { TLocation } from '../../services/types/types';
 import './App.scss';
-import {  Header  } from '../Header/Header';
+import { Header } from '../Header/Header';
 import { LoginPage } from '../../pages/LoginPage/LoginPage';
 import { Footer } from '../Footer/Footer';
-import DetailCard from '../DetailCard/DetailCard';
-import { TThemeProfile } from '../../services/types/types';
 
 export const App: FC = () => {
   const location = useLocation<TLocation>();
   return (
     <div className='app'>
-      <Header/>
+      <Header />
       <main className='main'>
         <Switch location={location}>
           <Route exact path="/">
-            <LoginPage/>
+            <LoginPage />
           </Route>
           <Route exact path="/maria">
             <h1>Привет, Мария</h1>
           </Route>
           <Route exact path="/vadim">
-            
-            <DetailCard theme={TThemeProfile.DARING} heading='HEADING' text='here should be something text about your hobby' />
+            <h1>Привет, Вадим</h1>
           </Route>
           <Route exact path="/evgeniy">
             <h1>Привет, Евгений</h1>
@@ -33,11 +30,11 @@ export const App: FC = () => {
             <h1>Привет, Евгения</h1>
           </Route>
           <Route exact path="/alexey">
-            <AlexeyM/>
+            <AlexeyM />
           </Route>
         </Switch>
       </main>
-      <Footer/>
+      <Footer />
     </div>
   )
 }
