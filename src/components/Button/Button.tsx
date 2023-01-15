ae93676c255798520cdf9bc7b88b3375c7cc0b75
marussia1996 <mariashirokova1996@gmail.com>
2023-01-15 13:43:04 +0400

    change buttton

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,9 +1,22 @@
 import React, { FC } from 'react'
-import '../Button/Button.scss'
+import { TButtonView } from '../../services/types/types';
+import styles from '../Button/Button.module.scss'
+import classnames from "classnames";
 
-type Props = { text: string; className: 'buttonLarge' | 'buttonSmall'; disabled: boolean;  onClick: ()=>void; type: 'button' | 'reset' | 'submit'}
-export const Button: FC<Props> = ({text, className, disabled, onClick, type}) => {
+let cx = classnames.bind(styles);
+type Props = { 
+  text: string; 
+  view: TButtonView; 
+  disabled?: boolean;  
+  onClick: ()=>void; 
+  type: 'button' | 'reset' | 'submit'
+}
+export const Button: FC<Props> = ({text, view, disabled, onClick, type}) => {
+  const cxButton = cx(styles.button, {
+    [styles['buttonLarge']]: view === TButtonView.LARGE,
+    [styles['buttonSmall']]: view === TButtonView.SMALL
+});
   return (
-    <button className={`button ${className}`} disabled={disabled} onClick={onClick} type={type}>{text}</button>
+    <button className={cxButton} disabled={disabled} onClick={onClick} type={type}>{text}</button>
   )
 }
