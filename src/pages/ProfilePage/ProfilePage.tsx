4ed4bd1d455533c3072bb4f8944e771e3cd2387d
marussia1996 <mariashirokova1996@gmail.com>
2023-01-16 17:17:42 +0400

    change LoginPage & create ProfilePage

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { InputTextArea } from '../../components/InputTextArea/InputTextArea'
+import stylesProfile from '../ProfilePage/ProfilePage.module.scss'
 
 export const ProfilePage = () => {
   return (
-    <div>ProfilePage</div>
+    <section className={`${stylesProfile.profilePage}`}>
+      <InputTextArea state={'other'}/>
+      <InputTextArea state={'quote'} label={'Девиз, цитата'}/>
+      <InputTextArea state={'other'} label={'Из какой сферы пришёл? Кем работаешь?'}/>
+      <InputTextArea state={'other'} label={'Почему решил учиться на веб-разработчика?'}/>     
+    </section>
   )
 }
