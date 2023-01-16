d3118e7ca18b832ecd98acd1f4a8b2f3929a9dbf
marussia1996 <mariashirokova1996@gmail.com>
2023-01-16 18:06:15 +0400

    change button

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,12 +1,11 @@
 import { Button } from '../../components/Button/Button'
-import { TButtonView } from '../../services/types/types'
 import stylesLogin from '../LoginPage/LoginPage.module.scss'
 //пока нет ответа о том, как должна вести себя шапка страницка будет такой)
 export const LoginPage = () => {
   return (
     <section className={`${stylesLogin.loginPage}`}>
       <h1 className={`${stylesLogin.title}`}>С кем я учусь?</h1>
-      <Button text='Войти с Яндекс ID' view={TButtonView.LARGE} onClick={()=>{console.log('send')}} disabled={false} type='submit'/>
+      <Button size={'Large'} onClick={(e)=>{console.log('send')}} disabled={false} htmlType={'submit'}>Войти с Яндекс ID</Button>
     </section>
   )
 }
