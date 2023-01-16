7f6668d7df39f353ddbfe241a4d450553f222efc
marussia1996 <mariashirokova1996@gmail.com>
2023-01-16 17:17:42 +0400

    change LoginPage & create ProfilePage

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,6 +1,6 @@
 import { Button } from '../../components/Button/Button'
 import stylesLogin from '../LoginPage/LoginPage.module.scss'
-//пока нет ответа о том, как должна вести себя шапка страницка будет такой)
+
 export const LoginPage = () => {
   return (
     <section className={`${stylesLogin.loginPage}`}>
