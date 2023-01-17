1e374736dec1e0419d20cb488f1370d0187e2ce4
Evgeniya Kostyrya <zhenya241191@gmail.com>
2023-01-17 18:53:15 +0300

    добавлен input file

--- src/components/AddFile/AddFile.tsx
+++ src/components/AddFile/AddFile.tsx
@@ -1,12 +1,32 @@
 import stylesAddFile from '../AddFile/AddFile.module.scss'
-import { Button } from '../Button/Button'
-//пока не знаю нужны ли здесь пропсы и какие
+
 export const AddFile = () => {
+
+  const onChange = (e: any) => {
+    let files = e.target.files;
+    console.log(files);
+    let reader = new FileReader();
+    reader.readAsDataURL(files[0]);
+    reader.onload = (res: any) => {
+      console.log(res.target.result);
+    };
+  }
   return (
     <div className={`${stylesAddFile.wrap}`}>
       <h2 className={`${stylesAddFile.title}`}>Добавить студентов</h2>
       <p className={`${stylesAddFile.content}`}>Чтобы добавить новых студентов, загрузите csv или xlsx файл: первая колонка должна содержать email студентов, вторая колонка — номер когорты.</p>
-      <Button size={'Small'} onClick={(e)=>{console.log('add')}} disabled={false} htmlType={'submit'}>Добавить студента</Button>
+      <input
+        type='file'
+        name='file'
+        id='file'
+        accept=".xlsx, .cvs"
+        className={stylesAddFile.input}
+        onChange={onChange}
+        required
+      />
+      <label htmlFor='file' className={stylesAddFile.label}>
+        <span className={stylesAddFile.button}>Выберите файл</span>
+      </label>
     </div>
   )
 }
