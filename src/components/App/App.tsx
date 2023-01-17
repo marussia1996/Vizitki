e78b59a9c30e5149ed7b503bf64216efee5f437d
marussia1996 <mariashirokova1996@gmail.com>
2023-01-17 18:13:02 +0400

    add tests api func

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -7,12 +7,23 @@ import { Header } from '../Header/Header';
 import { LoginPage } from '../../pages/LoginPage/LoginPage';
 import { Footer } from '../Footer/Footer';
 import Evgenys from "../EvgenyS/Evgenys";
-import { getUsers } from '../../utils/api';
+import { deleteComment, getComments, getProfiles, getUserProfile, getUserReactions, getUsers, patchUserProfile, postUser, postUserReactions, putUser, TUserProfileRaw } from '../../utils/api';
 
 export const App: FC = () => {
   const location = useLocation<TLocation>();
+  
   useEffect(()=>{
-    console.log(getUsers())
+    // console.log(getUsers());
+    // console.log(postUser('maria@gm.com', 'web+11'));
+    // console.log(putUser('maria@gm.com', 'web+11', 'abfccdaa23e0bd1c4448d2f3'));
+    // console.log(getComments());
+    //TODO: от сервера не приходит ответ (или я его не вижу), поэтому checkResponse функция выдает ошибку. я не знаю как это решить
+    //console.log(deleteComment('c824a2de0b675b0acb5a2923'));
+    // console.log(getProfiles());
+    // console.log(getUserProfile('abfccdaa23e0bd1c4448d2f3'));
+    // console.log(patchUserProfile('abfccdaa23e0bd1c4448d2f3'));
+    // console.log(getUserReactions('abfccdaa23e0bd1c4448d2f3'));
+    console.log(postUserReactions('abfccdaa23e0bd1c4448d2f3', "job", "occaecat"))
   })
   return (
     <div className='app'>
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,10 +1,9 @@
-//просто чтобы папка появилась
-// export const baseUrl: string = "http://localhost:3000";
-export const token = 'wertyu45678cfgh567`'
+//тестовый токен
+export const token = 'wertyu45678cfgh567'
 //проверка ответа от сервера
 const checkResponse = <T>(res: Response):Promise<T> => {
   if (res.ok) {
-    return res.json();
+    return res.json(); 
   }
   return Promise.reject(
     new Error(`Произошла ошибка со статус-кодом ${res.status}`)
@@ -14,22 +13,229 @@ const checkResponse = <T>(res: Response):Promise<T> => {
 const request = <T>(url: string, options: RequestInit): Promise<T> => {
   return fetch(url, options).then(res => checkResponse<T>(res))
 }
-type TUserRaw = {
-  _id: string,
-  createdAt: number,
-  updatedAt: number,
-  email: string,
-  cohort: string,
-  name: string
-}
 type TGetUsersRaw = {
   total: number,
   items: Array<TUserRaw>
 }
-//запрос данных
+type TUserRaw = {
+  _id: string,
+  createdAt: number,
+  updatedAt: number,
+  email:string,
+  cohort:string,
+  name:string
+};
+type TCommentRaw = {
+  text: string,
+  _id: string,
+  from: {
+    value: string
+  },
+  target: string, //это поле отвечает за то на что дали комментарий
+  to: {
+    _id: string,
+    name: string,
+    email: string
+  }
+}
+type TUserReactionRaw = {
+  emotion?: string,
+  text?: string,
+  _id?: string,
+  from: {
+    value: string
+  },
+  target: string
+}
+type TUserReactionsRaw = {
+  total: number,
+  items: Array<TUserReactionRaw>
+}
+type TGetCommentsRaw = {
+    total: number,
+    items: Array<TCommentRaw>,
+}
+type TProfileRaw = {
+  email: string,
+  cohort: string,
+  _id: string,
+  createdAt: number,
+  updatedAt: number,
+  profile: {
+    name: string,
+    photo: string,
+    city: {
+      name: string,
+      geocode: [
+        number,
+        number
+      ]
+    }
+  }
+}
+type TProfilesRaw = {
+  total: number,
+  items: Array<TProfileRaw>
+}
+export type TUserProfileRaw = {
+    email: string,
+    cohort: string,
+    _id: string,
+    createdAt: number,
+    updatedAt: number,
+    profile: {
+      name: {
+        value: string
+      },
+      photo: {
+        value: string
+      },
+      city: {
+        value: string
+      },
+      birthday: string,
+      quote: string,
+      telegram: string,
+      github: string,
+      template: string
+    },
+    info: {
+      hobby: {
+        value: string
+      },
+      status: {
+        value: string
+      },
+      job: {
+        value: string
+      },
+      edu: {
+        value: string
+      }
+    },
+    reactions: number
+}
+//запрос всех пользователей
 export const getUsers = async() => {
-  return request<TGetUsersRaw>(`/users`, {
-    headers: {'Authorization' : 'Bearer ' + token},
+  return request<TGetUsersRaw>('/users', {
+    headers: {'Content-Type': 'application/json', 'Authorization' : 'Bearer ' + token},
     method: "GET",
   });
 };
+//отправка пользователя
+export const postUser = async(email: string, cohort: string) =>{
+  return request<TUserRaw>('/users', {
+    method: 'POST',
+      headers: { 'Content-Type': 'application/json', 'Authorization' : 'Bearer ' + token },
+      body: JSON.stringify({
+          'email': email,
+          'cohort': cohort
+      }),
+    })
+}
+//изменение пользователя //id: the user id
+export const putUser = async(email: string, cohort: string, _id: string) =>{
+  return request<TUserRaw>(`/users/${_id}`, {
+    method: 'PUT',
+      headers: { 'Content-Type': 'application/json', 'Authorization' : 'Bearer ' + token },
+      body: JSON.stringify({
+          'email': email,
+          'cohort': cohort
+      }),
+    })
+}
+//получение комментариев
+export const getComments = async() => {
+  return request<TGetCommentsRaw>('/comments', {
+    headers: {'Content-Type': 'application/json', 'Authorization' : 'Bearer ' + token},
+    method: "GET",
+  });
+};
+
+//удаление комментария //id: user.reactions[]._id
+export const deleteComment = async(_id: string) => {
+  return request(`/comments/${_id}`, {
+    headers: {'Content-Type': 'text/plain', 'Authorization' : 'Bearer ' + token},
+    method: "DELETE",
+  });
+};
+
+//запрос профилей - по умолчанию возвращают профили из той же когорты, что и запрошенный пользователь, или ничего
+export const getProfiles = async() => {
+  return request<TProfilesRaw>('/profiles', {
+    headers: {'Content-Type': 'application/json', 'Authorization' : 'Bearer ' + token},
+    method: "GET",
+  });
+};
+
+//запрос профиля пользователя _id: the user id
+export const getUserProfile = async(_id: string) => {
+  return request<TUserProfileRaw>(`/profiles/${_id}`, {
+    headers: {'Content-Type': 'application/json', 'Authorization' : 'Bearer ' + token},
+    method: "GET",
+  });
+};
+//изменение профиля пользователя //id: the user id
+//TODO: надо определиться как передавать данные и какие и тп
+export const patchUserProfile = async(_id: string) =>{
+  return request<TUserProfileRaw>(`/profiles/${_id}`, {
+    method: 'PATCH',
+      headers: { 'Content-Type': 'application/json', 'Authorization' : 'Bearer ' + token },
+      body: JSON.stringify({
+        "profile": {
+          "name": "Ivan Ivanov",
+          "photo": "https://placehold.co/600",
+          "city": {
+            "name": "blabla",
+            "geocode": [
+              55.73433517114847,
+              37.59017466910319
+            ]
+          },
+          "birthday": "1978-06-16",
+          "quote": "cupidatat voluptate",
+          "telegram": "Ut ullamco ex do sit",
+          "github": "consequat nostrud",
+          "template": "Ut aliqua dolore do"
+        },
+        "info": {
+          "hobby": {
+            "text": "",
+            "image": null
+          },
+          "status": {
+            "text": "",
+            "image": null
+          },
+          "job": {
+            "text": "",
+            "image": null
+          },
+          "edu": {
+            "text": "",
+            "image": null
+          }
+        }
+      }),
+    })
+}
+//получение реакций профиля пользователя //id: the user id
+export const getUserReactions = async(_id: string) => {
+  return request<TUserReactionsRaw>(`/profiles/${_id}/reactions`, {
+    headers: {'Content-Type': 'application/json', 'Authorization' : 'Bearer ' + token},
+    method: "GET",
+  });
+};
+//отправка реакций профиля пользователя //id: the user id
+//TODO: чет не так
+export const postUserReactions = async(_id: string, target: string, text: string) => {
+  return request<TUserReactionsRaw>(`/profiles/${_id}/reactions`, {
+    headers: {'Content-Type': 'text/plain', 'Authorization' : 'Bearer ' + token},
+    method: "POST",
+    body: JSON.stringify({
+      "target": target,
+      "text": text
+    })
+  });
+};
+
