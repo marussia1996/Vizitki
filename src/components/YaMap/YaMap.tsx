b3f7a7000535709a9eccb20c52a405c2c1d5566c
Vadim Belousov <thevadiratti@yandex.ru>
2023-01-17 17:34:59 +0500

    Подготовить карту к получению внешних данных

--- src/components/YaMap/YaMap.tsx
+++ src/components/YaMap/YaMap.tsx
@@ -11,31 +11,37 @@ import {
   ZoomControl
 } from '@pbe/react-yandex-maps';
 import iconPlacemark from '../../images/YaMap/Placemark.svg';
+//FIXME Тестовый аватар, надо будет заменить на данные с сервера
 import avatar from '../../images/YaMap/avatar.png';
+//FIXME Тестовые данные, надо будет заменить на данные с сервера
+import { data } from './data';
 
 export default function YaMap() {
-
   return (
     <YMaps query={{ lang: 'ru_RU' }}>
       <main className='map'>
         <Map defaultState={{ center: [55.751574, 37.573856], zoom: 5 }} style={{ width: '100%', height: '100%' }}>
 
-          <Placemark
-            geometry={[55.684758, 37.738521]}
-            properties={{
-              balloonContent: `<div class= "balloon"><img class="imgBalloon" src = "${avatar}"></img><div><p class="balloonName">Шреков Шрек</p><p class='balloonCity'>Москва</p><div></div>`,
-              
-            }}
-            options={{
-              iconLayout: 'default#image',
-              iconImageHref: iconPlacemark,
-              iconImageSize: [46, 52],
-              openEmptyBalloon: true,
-              balloonOffset: [14, 0],
-              balloonMinHeight: 49,
-              balloonMinWidth: 163
-            }}
-          />
+          {data.map((student, i) => {
+            return (
+              <Placemark
+                geometry={student.geometry}
+                properties={{
+                  balloonContent: `<div class= "balloon"><img class="imgBalloon" src = "${avatar}"></img><div><p class="balloonName">${student.name}</p><p class='balloonCity'>${student.city}</p><div></div>`,
+
+                }}
+                options={{
+                  iconLayout: 'default#image',
+                  iconImageHref: iconPlacemark,
+                  iconImageSize: [46, 52],
+                  balloonOffset: [14, 0],
+                  balloonMinHeight: 49,
+                  balloonMinWidth: 163
+                }}
+                key={i}
+              />
+            )
+          })}
 
           <SearchControl
             options={{
