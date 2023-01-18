742551e4ca496bcfe88f4004550979d83b4e7992
Evgeniya Kostyrya <zhenya241191@gmail.com>
2023-01-18 14:54:42 +0300

    fix

--- src/components/AddFile/AddFile.tsx
+++ src/components/AddFile/AddFile.tsx
@@ -1,15 +1,20 @@
-import stylesAddFile from '../AddFile/AddFile.module.scss'
+import stylesAddFile from '../AddFile/AddFile.module.scss';
+import { useState } from 'react';
 
 export const AddFile = () => {
+
+  const [files, setFiles] = useState(null);
 
   const onChange = (e: any) => {
     let files = e.target.files;
     console.log(files);
-    let reader = new FileReader();
-    reader.readAsDataURL(files[0]); //TODO решить какой метод использовать для чтения файла, в каком виде нужны данные
+    const reader = new FileReader();
     reader.onload = (res: any) => {
+      setFiles(res.target.result)
       console.log(res.target.result);
     };
+    reader.readAsText(files[0]); //TODO решить какой метод использовать для чтения файла, в каком виде нужны данные
+    console.log(JSON.stringify(files));
   }
   return (
     <div className={`${stylesAddFile.wrap}`}>
