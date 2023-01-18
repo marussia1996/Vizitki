65850467ca29a07765c51ae8e1fd3747fac8f345
forfrontskill <avmakarov.mail@yandex.ru>
2023-01-18 23:24:01 +0300

    Верстка страницы с таблицами (AdminPage)

--- src/pages/AdminPage/AdminPage.tsx
+++ src/pages/AdminPage/AdminPage.tsx
@@ -1,27 +1,53 @@
-import React from "react";
-import { Link, Route, Switch, useRouteMatch } from "react-router-dom";
+import React, { ChangeEvent, useState } from "react";
+import { Route, Switch, useRouteMatch } from "react-router-dom";
 import { AddFile } from "../../components/AddFile/AddFile";
 import CommentsTable from "../../components/CommentsTable/CommentsTable";
+import InputFilter from "../../components/InputFilter/InputFilter";
 import StudentTable from "../../components/StudensTable/StudensTable";
 import { SwitchInfo } from "../../components/Switch/Switch";
 
-// import styles from './AdminPage.module.scss';
+import styles from './AdminPage.module.scss';
 
 const AdminPage = () => {
 
     let { path } = useRouteMatch();
 
-    console.log('Render AdminPage');
+    const [filter, setFilter] = useState<string>()
+
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setFilter(event.target.value);
+    }
+
+    const handleClearFilter = () => {
+        setFilter('');
+    }
+
     return (
-        <section>
-            <SwitchInfo /><AddFile/>
-            <Link to='/alexey'>ADMIN</Link>
+        <section className={styles.AdminPage}>
+            <div className={styles.Filter}>
+                <SwitchInfo />
+                <InputFilter
+                    title="Фильтровать"
+                    value={filter}
+                    placeHolder="По имени или фамилии или почте или номеру когорты (введите любой из этих параметров)"
+                    onChange={handleChange}
+                    onClear={handleClearFilter}
+                    mix={styles.InputTableFilter}
+                />
+            </div>
             <Switch>
                 <Route path={path} exact>
-                    <CommentsTable />
+                    <div className={styles.TableContent}>
+                        <CommentsTable />
+                    </div>
                 </Route>
                 <Route path={`${path}/users`} exact>
-                    <StudentTable />
+                    <div className={styles.AddFile}>
+                        <AddFile />
+                        </div>
+                    <div className={styles.TableContent}>
+                        <StudentTable />
+                    </div>
                 </Route>
             </Switch>
         </section>
