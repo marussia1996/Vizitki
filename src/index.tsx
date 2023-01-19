fa9ad706a7aa3180f5a1a41d4671a6a2ec8ed4f6
marussia1996 <mariashirokova1996@gmail.com>
2023-01-19 14:27:10 +0400

    change index

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { App } from './components/App/App';
+import ReactDOM from 'react-dom/client';
 import './index.scss';
+import {App} from './components/App/App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router} from 'react-router-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+root.render(
+  <Router>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+  </Router>
 );
 
 reportWebVitals();
+
