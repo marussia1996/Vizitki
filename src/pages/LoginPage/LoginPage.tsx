cca242dee14b9429febffbff2dd270cdee827956
marussia1996 <mariashirokova1996@gmail.com>
2023-01-20 21:19:58 +0400

    получение токена, заливка данных о пользователе

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,28 +1,49 @@
 import { FC } from 'react'
 import { Redirect, useLocation} from 'react-router'
+import { TLocation } from '../../services/types/types'
 import { Button } from '../../shared/Button/Button'
+import { redirectToYandex } from '../../utils/auth'
 import stylesLogin from '../LoginPage/LoginPage.module.scss'
-type LoginPageProps = {
-  response_type: string,
-  client_id: string,
-  redirect_uri: string
-}
 
-export const LoginPage:FC<LoginPageProps> = ({response_type, client_id, redirect_uri}) => {
-  
-  //клик по кнопке перенаправляет на страницу регистрации через яндекс
-  const handleClick = () =>{
-    window.location.assign(`https://oauth.yandex.ru/authorize?response_type=${response_type}&client_id=${client_id}&redirect_uri=${redirect_uri}`);
+
+export const LoginPage = () => {
+  const location = useLocation<TLocation>();
+  //если хаш есть, то есть ответ от яндекса пришел, то достаем токен
+  if(location.hash){
+    console.log(location.hash)
+    //время жизни токена
+    const timeToken: RegExpMatchArray | null = location.hash.match(/expires_in=(\d+)/);
+    //время на момент прихода токена
+    const timeNow: number = Math.floor(new Date().getTime() / 1000);
+    //через сколько токен надо обновлять
+    const accessTokenTimeRefresh: string = String(timeToken && (Number(timeToken[1]) + Number(timeNow)));
+    accessTokenTimeRefresh && localStorage.setItem('accessTokenTimeRefresh', accessTokenTimeRefresh);
+    // timeToken && localStorage.setItem('timeToken', timeToken[1])
+    const accessToken = location.hash.match(/#access_token=([\w-]+)/);
+    accessToken && localStorage.setItem('accessToken', accessToken[1])
+    //фейк на получение данных о пользователе
+    const admin = {
+      email: 'hjkll',
+      name: 'blblblb',
+      curator: 'web-12', 
+      image: '' 
+    }
+    const student = {
+      email: 'hjkll',
+      name: 'blblblb',
+      cohort: 'web-12',
+      image: '' 
+    }
+    localStorage.setItem('user', JSON.stringify(admin))
   }
   
-  
-  if(localStorage.getItem('access_token')){
+  if(localStorage.getItem('user')){
     return <Redirect to='/'/>
   }
   return (
     <section className={`${stylesLogin.loginPage}`}>
       <h1 className={`${stylesLogin.title}`}>С кем я учусь?</h1>
-      <Button size={'Large'} onClick={handleClick} disabled={false} htmlType={'submit'}>Войти с Яндекс ID</Button>
+      <Button size={'Large'} onClick={redirectToYandex} disabled={false} htmlType={'submit'}>Войти с Яндекс ID</Button>
     </section>
   )
 }
