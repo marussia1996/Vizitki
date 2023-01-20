5d0f1cdcff5d873fdedd1b340dceaedd3b102455
marussia1996 <mariashirokova1996@gmail.com>
2023-01-20 21:19:29 +0400

    чутка добавила редиректы

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -8,7 +8,6 @@ import { Redirect } from 'react-router';
 import { getUsers } from '../../utils/api';
 
 export const MainPage = () => {
-
     getUsers()
     .then((res)=>{
       localStorage.setItem('users', JSON.stringify(res))
