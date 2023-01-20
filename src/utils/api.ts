fa5d5f8f662b101825233abeda4cd89f2aafb86d
marussia1996 <mariashirokova1996@gmail.com>
2023-01-20 21:23:19 +0400

    добавила в хедеры всем запросам токен

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,6 @@
 import { BaseFiedsRaw, InfoItemsRaw, ProfileRaw, TGetCommentsRaw, TGetProfilesRaw, TGetUsersRaw, TUserReactionsRaw, UserAccountRaw, UserWithProfileRaw } from "../services/types/types";
+import { getUserToken } from "./auth";
 
-//тестовый токен
-export const token = 'wertyu45678cfgh567'
 //проверка ответа от сервера
 const checkResponse = <T>(res: Response, readBody: boolean = true):Promise<T | void> => {
   if (res.ok) {
@@ -16,24 +15,18 @@ const checkResponse = <T>(res: Response, readBody: boolean = true):Promise<T | v
     new Error(`Произошла ошибка со статус-кодом ${res.status}`)
   );
 };
-const response_type='code';
-const client_id ='e0363480e511432f87948725fe869e7f';
-const redirect_uri = 'http%3A%2F%2Flocalhost%3A3000%2Flogin';
-//получение jwt 
-export const GetJWTToken = async() =>{
-  return fetch(`https://oauth.yandex.ru/authorize?response_type=${response_type}&client_id=${client_id}&redirect_uri=${redirect_uri}`,)
-}
+const token = getUserToken();
+
 //универсальная функция запроса с проверкой
 const request = <T>(url: string, options: RequestInit, readBody: boolean = true): Promise<T | void> => {
+  options.headers = {...options.headers, 'Authorization' : 'Bearer ' + token}
   return fetch(url, options).then(res => checkResponse<T>(res, readBody))
 }
-//TODO: надо разобраться с типами
-
 
 //запрос всех пользователей
 export const getUsers = async() => {
   return request<TGetUsersRaw>('/users', {
-    headers: {'Content-Type': 'application/json', 'Authorization' : 'Bearer ' + token},
+    headers: {'Content-Type': 'application/json'},
     method: "GET",
   });
 };
@@ -41,7 +34,7 @@ export const getUsers = async() => {
 export const postUser = async(email: string, cohort: string) =>{
   return request<BaseFiedsRaw & UserAccountRaw & {name: string}>('/users', {
     method: 'POST',
-      headers: { 'Content-Type': 'application/json', 'Authorization' : 'Bearer ' + token },
+      headers: { 'Content-Type': 'application/json'},
       body: JSON.stringify({
           'email': email,
           'cohort': cohort
@@ -52,7 +45,7 @@ export const postUser = async(email: string, cohort: string) =>{
 export const putUser = async(email: string, cohort: string, _id: string) =>{
   return request<BaseFiedsRaw & UserAccountRaw & {name: string}>(`/users/${_id}`, {
     method: 'PUT',
-      headers: { 'Content-Type': 'application/json', 'Authorization' : 'Bearer ' + token },
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
           'email': email,
           'cohort': cohort
@@ -62,7 +55,7 @@ export const putUser = async(email: string, cohort: string, _id: string) =>{
 //получение комментариев
 export const getComments = async() => {
   return request<TGetCommentsRaw>('/comments', {
-    headers: {'Content-Type': 'application/json', 'Authorization' : 'Bearer ' + token},
+    headers: {'Content-Type': 'application/json'},
     method: "GET",
   });
 };
@@ -70,7 +63,7 @@ export const getComments = async() => {
 //удаление комментария //id: user.reactions[]._id
 export const deleteComment = async(_id: string) => {
   return request(`/comments/${_id}`, {
-    headers: {'Content-Type': 'text/plain', 'Authorization' : 'Bearer ' + token},
+    headers: {'Content-Type': 'text/plain'},
     method: "DELETE",
   }, false);
 };
@@ -78,7 +71,7 @@ export const deleteComment = async(_id: string) => {
 //запрос профилей - по умолчанию возвращают профили из той же когорты, что и запрошенный пользователь, или ничего
 export const getProfiles = async() => {
   return request<TGetProfilesRaw>('/profiles', {
-    headers: {'Content-Type': 'application/json', 'Authorization' : 'Bearer ' + token},
+    headers: {'Content-Type': 'application/json'},
     method: "GET",
   });
 };
@@ -86,7 +79,7 @@ export const getProfiles = async() => {
 //запрос профиля пользователя _id: the user id
 export const getUserProfile = async(_id: string) => {
   return request<BaseFiedsRaw & UserWithProfileRaw & {reactions: number}>(`/profiles/${_id}`, {
-    headers: {'Content-Type': 'application/json', 'Authorization' : 'Bearer ' + token},
+    headers: {'Content-Type': 'application/json'},
     method: "GET",
   });
 };
@@ -94,7 +87,7 @@ export const getUserProfile = async(_id: string) => {
 export const patchUserProfile = async(_id: string, data: {profile: ProfileRaw, info: InfoItemsRaw}) =>{
   return request<BaseFiedsRaw & UserWithProfileRaw & {reactions: number}>(`/profiles/${_id}`, {
     method: 'PATCH',
-      headers: { 'Content-Type': 'application/json', 'Authorization' : 'Bearer ' + token },
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
         data
       }),
@@ -103,14 +96,14 @@ export const patchUserProfile = async(_id: string, data: {profile: ProfileRaw, i
 //получение реакций профиля пользователя //id: the user id
 export const getUserReactions = async(_id: string) => {
   return request<TUserReactionsRaw>(`/profiles/${_id}/reactions`, {
-    headers: {'Content-Type': 'application/json', 'Authorization' : 'Bearer ' + token},
+    headers: {'Content-Type': 'application/json'},
     method: "GET",
   });
 };
 //отправка реакций профиля пользователя //id: the user id
 export const postUserReactions = async(_id: string, comment: {target: string, text: string} | {target: string, emotion: string}) => {
   return request<TUserReactionsRaw>(`/profiles/${_id}/reactions`, {
-    headers: {'Content-Type': 'text/plain', 'Authorization' : 'Bearer ' + token},
+    headers: {'Content-Type': 'text/plain'},
     method: "POST",
     body: JSON.stringify({
       comment
