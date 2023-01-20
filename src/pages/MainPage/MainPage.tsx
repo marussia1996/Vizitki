eb30c8f15bdeac852acab4dd3eecae477a843a10
marussia1996 <mariashirokova1996@gmail.com>
2023-01-20 21:19:29 +0400

    чутка добавила редиректы

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,6 +1,18 @@
-import React from 'react'
+import { Redirect } from 'react-router';
+import { getUsers } from '../../utils/api';
 
 export const MainPage = () => {
+  const userRaw = localStorage.getItem('user');
+  if(userRaw && userRaw.includes('curator')){
+    return <Redirect to='/admin'/>
+  }
+    getUsers()
+    .then((res)=>{
+      localStorage.setItem('users', JSON.stringify(res))
+    })
+    .catch(()=>{
+      console.log('err')
+    })
   return (
     <div>MainPage</div>
   )
