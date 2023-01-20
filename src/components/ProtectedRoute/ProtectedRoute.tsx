4f3934cdd051557c72f2561d6d553298797179c3
marussia1996 <mariashirokova1996@gmail.com>
2023-01-20 13:41:19 +0400

    получение access_token

--- src/components/ProtectedRoute/ProtectedRoute.tsx
+++ src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -4,7 +4,7 @@ import { FC, ReactNode } from 'react';
 export const ProtectedRoute: FC<RouteProps & {children?: ReactNode}> = ({ children, ...rest }) => {
   //TODO: надо исправить то откуда берется значение зареган пользователь или нет
   //TODO: скорей всего сюда же надо запихнуть проверку пользователь является куратором или нет, если он куратор переход на страницу /admin
-    const user = 'ghjk';
+    const user = localStorage.getItem('access_token');
     const location = useLocation();
     return (
         <Route
