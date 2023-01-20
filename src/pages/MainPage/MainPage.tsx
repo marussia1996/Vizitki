586b353b9423a5628aec30265ba973b0b5e5599f
Vadim Belousov <thevadiratti@yandex.ru>
2023-01-20 14:24:26 +0500

    расположить элементы

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,10 +1,23 @@
 import './MainPage.scss';
 import { NavLink } from "react-router-dom"
+import { InputSearch } from '../../shared/inputs/InputSearch/InputSearch';
+import { city } from '../../shared/inputs/InputSearch/test'; //TODO ТЕСТОВЫЕ ДАННЫЕ. Убрать
+import { UserCard } from '../../components/UserCard/UserCard';
 
 export const MainPage = () => {
   return (
     <div className="page">
-      <NavLink to="/map">Посмотреть на карте</NavLink>
+      <InputSearch options={city} />
+      <div className='linkCnt'>
+        <NavLink to="/map" className='link'>Посмотреть на карте</NavLink>
+      </div>
+      <div className='cardsCnt'>
+        <UserCard />
+        <UserCard />
+        <UserCard />
+        <UserCard />
+        <UserCard />
+      </div>
     </div>
   )
 }
