e774ed7a4f898d8b3d80145c1c67f7260304a934
Vadim Belousov <thevadiratti@yandex.ru>
2023-01-20 14:55:36 +0500

    добавить loader

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -3,6 +3,7 @@ import { NavLink } from "react-router-dom"
 import { InputSearch } from '../../shared/inputs/InputSearch/InputSearch';
 import { city } from '../../shared/inputs/InputSearch/test'; //TODO ТЕСТОВЫЕ ДАННЫЕ. Убрать
 import { UserCard } from '../../components/UserCard/UserCard';
+import Loader from '../../components/Loader/Loader';
 
 export const MainPage = () => {
   return (
@@ -17,6 +18,9 @@ export const MainPage = () => {
         <UserCard />
         <UserCard />
         <UserCard />
+      </div>
+      <div className='loaderCnt'>
+        <Loader />
       </div>
     </div>
   )
