cbeaa7b77744ae820c09f669a2f08b4905121d6a
marussia1996 <mariashirokova1996@gmail.com>
2023-01-21 17:43:14 +0400

    добавила показ ошибки

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -8,6 +8,7 @@ import stylesProfile from '../ProfilePage/ProfilePage.module.scss'
 import InputText from '../../shared/inputs/InputText/InputText';
 import { Button } from '../../shared/Button/Button';
 import { InputSearch } from '../../shared/inputs/InputSearch/InputSearch';
+import { stat } from 'fs';
 
 export const city = [
   'Абаза', 
@@ -27,8 +28,11 @@ export const theme = [
 ]
 
 type TInputState = {
+  photo: string,
   birthday?: Date,
+  errBirthday: boolean,
   city: string,
+  errCity: boolean,
   telegram: string,
   github: string,
   template: string,
@@ -41,9 +45,14 @@ type TInputState = {
   eduText: string,
 }
 export const ProfilePage = () => {
+  const userRaw = localStorage.getItem('user');
+  const user = userRaw && JSON.parse(userRaw);
   //TODO: запрос данных пользователя 
   const [state, setState] = useState<TInputState>({
+    photo: '',
     birthday: undefined,
+    errBirthday: false,
+    errCity: false,
     telegram: '',
     github: '',
     city: '',
@@ -56,11 +65,36 @@ export const ProfilePage = () => {
     jobText: '',
     eduText: ''
   });
+  const validity = () =>{
+    let valid = true;
+    if(state.birthday === undefined && state.city === ''){
+      valid = false;
+      setState({...state, ['errBirthday']:true, ['errCity']:true})
+    }else if(state.birthday === undefined){
+      valid = false;
+      setState({...state, ['errBirthday']:true, ['errCity']:false})
+    }else if(state.city === ''){
+      valid = false;
+      setState({...state, ['errBirthday']:false, ['errCity']:true})
+    }else{
+      valid = true;
+      setState({...state, ['errBirthday']:false, ['errCity']:false})
+    }
+    return valid;
+  }
   //TODO: валидация по заполнению поля birthday и city
   const handleSubmit = (e: FormEvent<HTMLFormElement>) =>{
     e.preventDefault();
+    if(validity()){
+      console.log('valid')
+      
+    }
+    else{
+      console.log('no valid')
+      console.log(state)
+    }
     //TODO: отправка формы на сервер
-    console.log(state)
+    
   }
   const onChange = (e: React.ChangeEvent<HTMLSelectElement> | React.ChangeEvent<HTMLInputElement> | TInputChange<any>) => {
     setState({...state, [e.target.name]:e.target.value})
@@ -68,17 +102,13 @@ export const ProfilePage = () => {
   return (
     <section className={`${stylesProfile.profilePage}`}>
       <form className={`${stylesProfile.formProfile}`} onSubmit={handleSubmit} noValidate>
-        {/* TODO: надо исправить фото загрузку, чтобы изменения в этом компоненте появлялись, чтобы пропсы пробросить можно было image и тп */}
-        <PhotoUpload/>
-        {/* если поле не заполнено передать ошибку */}
-        <InputDay error={state.birthday ? '' : 'Поле обязательно для заполнения'} name={'birthday'} date={state.birthday} labelText={'Дата рождения *'} maxDate={new Date(Date.UTC(2022, 1, 5))}
+        <PhotoUpload name={'photo'} value={state.photo} onFileChange={onChange}/>
+        <InputDay error={state.errBirthday ? 'Поле обязательно для заполнения' : ''} name={'birthday'} date={state.birthday} labelText={'Дата рождения *'} maxDate={new Date(Date.UTC(2022, 1, 5))}
         onDateChange={onChange}/>
-        {/* TODO: исправить компонент поиска (проброс пропсов, списка опций, ошибка и тд), подправить стили */}
-        <InputSearch options={city}/>
+        <InputSearch labelText={'Выберите город *'} error={state.errCity ? 'Поле обязательно для заполнения' : ''} options={city} value={state.city} onChange={onChange} name={'city'}/>
         <InputText name={'telegram'} labelText={'Ник в телеграмм'} onChange={onChange} />
         <InputText name={'github'} labelText={'Ник в гитхабе'} onChange={onChange} />
-        {/* TODO: исправить компонент поиска */}
-        <InputSearch options={theme}/>
+        <InputSearch labelText='Выберите шаблон' options={theme} value={state.template} onChange={onChange} name={'template'}/>
         <InputTextArea name={'quote'} labelText={'Девиз, цитата'} value={state.quote} onChange={onChange} maxLength={100} rows={4}/>
         <div>
           <InputFile name={'hobbiesFile'} labelText={'Увлечения, досуг, интересы'} description={'Рекомендуемый размер фото 230х129'} onFileChange={onChange} />
