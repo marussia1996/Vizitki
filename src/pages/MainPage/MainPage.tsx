e0fb636e9626dd72b75925e4cdf5d6ee076032b6
Vadim Belousov <thevadiratti@yandex.ru>
2023-01-22 14:32:59 +0500

    подключить отображение профилей

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,12 +1,22 @@
 import './MainPage.scss';
+import { useEffect, useState } from 'react';
 import { NavLink } from "react-router-dom"
 import { InputSearch } from '../../shared/inputs/InputSearch/InputSearch';
 import { city } from '../../shared/inputs/InputSearch/test'; //TODO ТЕСТОВЫЕ ДАННЫЕ. Убрать
 import { UserCard } from '../../components/UserCard/UserCard';
 import Loader from '../../components/Loader/Loader';
-import { getUsers } from '../../utils/api';
+import { getProfiles } from '../../utils/api';
+import { TGetProfilesRaw } from '../../services/types/types';
 
 export const MainPage = () => {
+  const [state, setState] = useState<TGetProfilesRaw | void  | null>(null);
+
+  useEffect(() => {
+    getProfiles().then(res => setState(res));
+  }, []);
+
+  console.log(state);
+
   return (
     <div className="page">
       <div className='content'>
@@ -15,11 +25,12 @@ export const MainPage = () => {
           <NavLink to="/map" className='link'>Посмотреть на карте</NavLink>
         </div>
         <div className='cardsCnt'>
-          <UserCard />
-          <UserCard />
-          <UserCard />
-          <UserCard />
-          <UserCard />
+          {state?.items.map((student) => {
+            const {name, photo, city} = student.profile;
+            return (
+              <UserCard name={name} photo={photo} city={city.name} />
+            )
+          })}
         </div>
       </div>
       <div className='loaderCnt'>
