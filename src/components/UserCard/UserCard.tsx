bd947dd03d51ed2b6a6fbfbfbf5725f93ca8ca06
Vadim Belousov <thevadiratti@yandex.ru>
2023-01-23 20:16:30 +0500

    небольшие правки

--- src/components/UserCard/UserCard.tsx
+++ src/components/UserCard/UserCard.tsx
@@ -49,7 +49,7 @@ export const UserCard: FC<TProps> = ({ name, photo, city, id }) => {
       <div className={styles.infoWrap} onClick={openProfile}>
         <p className={styles.name}>{name}</p>
         <p className={styles.city}>{city}</p>
-        {/* отображается только для админа */}
+        {/* TODO отображается только для админа, переделать когда будут данные о пользователе */}
         <p className={styles.messages}>{state?.total + ' сообщений'}</p>
       </div>
       {isOpenFeedback && <Feedback comments={profileComments} id={id} />}
