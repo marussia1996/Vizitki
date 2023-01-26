20e4017920f880daadb0e5ffb8e0e35a0138f490
Самойлов Евгений Александрович <easamoilov@gmail.com>
2023-01-26 17:24:05 +0300

    fix login page

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,17 +1,17 @@
-import {useContext, useEffect} from 'react'
+import {useEffect} from 'react'
 import {useLocation} from 'react-router'
-import {AuthContext} from '../../services/AuthContext'
 import {RoleType} from '../../services/types/types'
 import {Button} from '../../shared/Button/Button'
 import {redirectToYandex} from '../../utils/auth'
 import stylesLogin from '../LoginPage/LoginPage.module.scss'
 import {fakeUser, parseAuthHash} from "../../utils/helpers";
+import {useAuth} from "../../hooks/useAuth";
 
 
 export const LoginPage = () => {
   const location = useLocation();
 
-  const {login, updateRole, role} = useContext(AuthContext);
+  const {login, updateRole, role} = useAuth();
 
   useEffect(() => {
     if (location.hash) {
